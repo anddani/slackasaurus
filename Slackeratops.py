bb8694111c58d7be8c5cc3b96f875c57f44fd816
André Danielsson <andre.danielsson@bontouch.com>
2017-07-01 14:50:49 +0200

    Slackeratops added for slack communication

---
+++ Slackeratops.py
@@ -0,0 +1,18 @@
+import json
+try:
+    import urllib.request as urlrequest
+except ImportError:
+    import urllib2 as urlrequest
+
+
+class Slackeratops():
+
+    def __init__(self, url):
+        self.url = url
+
+    def post(self, data):
+        request = urlrequest.Request(self.url, json.dumps(data))
+        request.add_header('Content-Type',  'application/json')
+
+        response = urlrequest.urlopen(request)
+        return response
--- slackasaurus.py
+++ slackasaurus.py
@@ -1,17 +1,54 @@
 from flask import Flask
-from flask import request
 from flask import make_response
+from flask import request
+
+from slackeratops import Slackeratops
+
 import json
+import logging
+import os
+
+"""
+TODO:
+    - Have slack_post() return the appropriate response
+    - Parse request from API.AI to find user, channel and message
+"""
+
+slack = Slackeratops(os.environ["SLACK_URL"])
+logging.basicConfig(level=logging.DEBUG)
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['POST'])
-def post_root():
-    body = json.dumps(request.get_json(silent=True, force=True), indent=4)
+def slack_post():
+    request_body = request.data
 
-    print('POST body:')
-    print(body)
-    response = make_response(body)
+    logging.debug('POST body:')
+    logging.debug(request_body)
+
+    # Parse the request body to find the user, channel and message.
+    parsed_request = parse_request(json.loads(request_body))
+    print(parsed_request['text'])
+
+    if 'text' in parsed_request:
+        logging.debug(parsed_request)
+        slack.post(parsed_request)
+
+    # TODO: Appropriate response
+    # For now, we return the parsed request (what is sent to slack)
+    response_body = json.dumps(parsed_request)
+
+    logging.debug('response body:')
+    logging.debug(response_body)
+
+    response = make_response(response_body)
     response.headers['Content-Type'] = 'application/json'
+
     return response
+
+
+def parse_request(request):
+    if request is None:
+        return {}
+    return request
